8ca3b759f70995fd841da702a2b390a084b7f93e
prismleong <981591636@qq.com>
2019-05-23 23:36:39 +0800

    Add files via upload

--- KNN.h
+++ KNN.h
@@ -5,18 +5,18 @@ using namespace cv;
 using namespace std;
 class KNN {
 public:
-	KNN(pic *p);
+	KNN(pic* p);
 	//输入矩阵
 	vector<double> mat2dot(Mat mt);
 	void train();
 	int predict(int K, int label, int index);
-	void test(int K,int num);
+	void test(int K, int num);
 private:
 	//28x28 分成 4x4个7x7的小块
 	//每块取平均灰度
 	vector<vector<vector<double>>> model;
-	double distance(vector<double> &dot1,vector<double> &dot2);
-	pic *p;
+	double distance(vector<double>& dot1, vector<double>& dot2);
+	pic* p;
 };
 
 #endif
--- MNIST.cpp
+++ MNIST.cpp
@@ -7,9 +7,18 @@
 
 using namespace std;
 using namespace cv;
-
+//KNN k=9 890  0.796067
+//KNN k=8 890  0.802921
+//KNN k=7 890  0.804270
+//KNN k=6 890  0.813708
+//KNN K=5 890  0.825281
+//KNN K=4 890  0.831573
+//KNN k=3 890  0.849438
+//KNN k=2 890  0.860899
+//KNN k=1 890  1.000000
 int main()
 {
+	ios::sync_with_stdio(false);
 	pic *P = new pic(GetStdHandle(STD_OUTPUT_HANDLE));
 	KNN knn(P);
 	knn.train();
