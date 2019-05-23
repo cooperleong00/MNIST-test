a23c03b9bcbfa699460824e35faaa84b135ce48f
prismleong <981591636@qq.com>
2019-05-23 21:20:41 +0800

    Add files via upload

---
+++ MNIST.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include "pch.h"
+#include "pic.h"
+#include "KNN.h"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	pic *P = new pic(GetStdHandle(STD_OUTPUT_HANDLE));
+	KNN knn(P);
+	knn.train();
+	//int lb, idx;
+	//while (cin >> lb >> idx) {
+	//	cout << "result: "<<knn.predict(7, lb, idx) << endl;
+	//}
+	int k, num;
+	while (cin >> k >> num) {
+		knn.test(k, num);
+	}
+	return 0;
+}
+
+
